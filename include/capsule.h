258a29edc13a9d04f2d74476a022747d29ce8ed8
melis_m <matteo.melis@epitech.eu>
2016-04-19 13:25:40 +0200

    added correct header for capsule parsing

--- include/capsule.h
+++ include/capsule.h
@@ -5,28 +5,34 @@
 ** Login   <grange_c@epitech.net>
 **
 ** Started on  Fri Apr 15 17:49:05 2016 Benjamin Grange
-** Last update Fri Apr 15 18:05:08 2016 Benjamin Grange
+** Last update Fri Apr 15 23:54:48 2016 Matteo Melis
 */
 
 #ifndef CAPSULE_H_
 # define CAPSULE_H_
 
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
 # include <stdio.h>
+# include <ctype.h>
 
 typedef struct		s_capsule
 {
   const char		*file_name;
-  FILE			*file;
+  FILE			*fp;
+  char			*buffer;
 }			t_capsule;
 
 /* Basic capsule file */
 
 t_capsule		*capsule_load(const char *file_name);
 void			capsule_del(t_capsule *capsule);
-void			*capsule_get_data(t_capsule *, const char *, int);
+char			*capsule_get_data(t_capsule *, const char *, int);
 
 /* Core capsule functions */
-
-// TODO
 
 #endif /* !CAPSULE_H_ */
