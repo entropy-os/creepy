21fd445ef9a4f614940c57e630ef839480a4664e
Benjamin Grange <benjamin.grange@epitech.eu>
2016-04-24 15:52:52 +0000

    Improved verbose (added vaargs)

--- source/utils/utils_verbose.c
+++ source/utils/utils_verbose.c
@@ -5,13 +5,21 @@
 ** Login   <grange_c@epitech.net>
 **
 ** Started on  Sun Apr 24 15:37:24 2016 Benjamin Grange
-** Last update Sun Apr 24 15:40:33 2016 Benjamin Grange
+** Last update Sun Apr 24 15:50:52 2016 Benjamin Grange
 */
 
+#include <stdarg.h>
 #include "creepy.h"
 
-void			verbose(t_creepy *creepy, const char *fmt)
+void			verbose(t_creepy *creepy, const char *fmt, ...)
 {
+  va_list		va;
+
+  va_start(va, fmt);
   if (creepy->params.verbose)
-    printf("%s\n", fmt);
+    {
+      vprintf(fmt, va);
+      printf("\n");
+    }
+  va_end(va);
 }
